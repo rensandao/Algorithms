8d12d5171459a38d07afa83086ac9267c64facd7
rensandao <38071371+rensandao@users.noreply.github.com>
2019-04-10 21:20:45 +0800

    Update FindMaxSubArray.h

--- Introduction to Algorithms/FindMaxSubArray/FindMaxSubArray.h
+++ Introduction to Algorithms/FindMaxSubArray/FindMaxSubArray.h
@@ -4,10 +4,16 @@
 #include <vector>
 
 namespace AL {
-
+class Solution001 {
     std::vector<int> FindMaxCrossingSubArray(int A[], int low, int high, int mid);
     std::vector<int> FindMaxSubArray(int A[], int low, int high);
-
+}
+    
+class Solution002 {
+    std::vector<int> FindMaxCrossingSubArray(int A[], int low, int high, int mid);
+    std::vector<int> FindMaxSubArray(int A[], int high);
+}
+    
 }
 
 #endif // FINDMAXSUBARRAY_H
